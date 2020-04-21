07940e7b5d48e925da834fef370abc85405a3744
Alberto Aldana <albertoaldanar@hotmail.com>
2020-04-20 21:14:49 -0600

    Initialized Routek web application

---
+++ src/containers/Layout/sidebar/SidebarContent.jsx
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import SidebarLink from './SidebarLink';
+import SidebarCategory from './SidebarCategory';
+
+class SidebarContent extends Component {
+  static propTypes = {
+    changeToDark: PropTypes.func.isRequired,
+    changeToLight: PropTypes.func.isRequired,
+    onClick: PropTypes.func.isRequired,
+  };
+
+  hideSidebar = () => {
+    const { onClick } = this.props;
+    onClick();
+  };
+
+  render() {
+    const { changeToLight, changeToDark } = this.props;
+
+    return (
+      <div className="sidebar__content">
+        <ul className="sidebar__block">
+          <SidebarLink title="Equipos" icon="users" route="/teams" onClick={this.hideSidebar} />
+          <SidebarLink
+            title="Actividades"
+            icon="location"
+            route="/dashboard_e_commerce"
+            onClick={this.hideSidebar}
+          />
+          <SidebarLink
+            title="Proyectos"
+            icon="apartment"
+            route="/projects"
+            onClick={this.hideSidebar}
+          />
+          <SidebarLink
+            title="Registros"
+            icon="list"
+            route="/dashboard_crypto"
+            onClick={this.hideSidebar}
+          />
+          <SidebarLink
+            title="Configuración"
+            icon="cog"
+            route="/dashboard_booking"
+            onClick={this.hideSidebar}
+          />
+          <SidebarLink
+            title="Estadisticas"
+            icon="chart-bars"
+            route="/dashboard_mobile_app"
+            onClick={this.hideSidebar}
+          />
+          <SidebarLink
+            title="Chat"
+            icon="bubble"
+            route="/chat"
+            onClick={this.hideSidebar}
+          />
+          <SidebarCategory title="Vista" icon="magic-wand">
+            <button className="sidebar__link" type="button" onClick={changeToLight}>
+              <p className="sidebar__link-title">Claro</p>
+            </button>
+            <button className="sidebar__link" type="button" onClick={changeToDark}>
+              <p className="sidebar__link-title">Oscuro</p>
+            </button>
+          </SidebarCategory>
+        </ul>
+
+        <ul className="sidebar__block">
+          <SidebarLink title="Log Out" icon="exit" route="/log_in" />
+        </ul>
+        <ul className="sidebar__block">
+          <SidebarLink
+            title="Documentation"
+            icon="text-align-justify"
+            route="/documentation/introduction"
+            onClick={this.hideSidebar}
+          />
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default SidebarContent;
