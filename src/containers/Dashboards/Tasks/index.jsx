1cf398f9e43e635e6a0c72cfdf38ac3e64e093f3
Alberto Aldana <albertoaldanar@hotmail.com>
2020-04-25 21:25:52 -0600

    Initialized v1 activities module

--- src/containers/Dashboards/Tasks/index.jsx
+++ src/containers/Dashboards/Tasks/index.jsx
@@ -1,78 +1,97 @@
 import React, { PureComponent } from 'react';
-import { Col, Container, Row } from 'reactstrap';
+import {
+  Button, Card, CardBody, Container, Col, Row, Nav, NavItem, NavLink, TabContent, TabPane,
+} from 'reactstrap';
+import classnames from 'classnames';
 import { withTranslation } from 'react-i18next';
+import UserAddIcon from 'mdi-react/UserAddIcon';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import TotalProducts from './components/TotalProducts';
-import TotalProfit from './components/TotalProfit';
-import OrdersToday from './components/OrdersToday';
-import Subscriptions from './components/Subscriptions';
-import TopSellingProducts from './components/TopSellingProducts';
-import BasicCard from './components/BasicCard';
-import SalesStatistic from './components/SalesStatistic';
-import RecentOrders from './components/RecentOrders';
-import ProductSales from './components/ProductSales';
-import NewOrders from './components/NewOrders';
-import SalesStatistisBar from './components/SalesStatistisBar';
-import MyTodos from './components/MyTodos';
-import Emails from './components/Emails';
-import SalesReport from './components/SalesReport';
-import ShortReminders from './components/ShortReminders';
-import { deleteNewOrderTableData } from '../../../redux/actions/newOrderTableActions';
-import { NewOrderTableProps } from '../../../shared/prop-types/TablesProps';
-import { RTLProps } from '../../../shared/prop-types/ReducerProps';
 
 class Tasks extends PureComponent {
   static propTypes = {
-    newOrder: NewOrderTableProps.isRequired,
-    dispatch: PropTypes.func.isRequired,
-    rtl: RTLProps.isRequired,
+    t: PropTypes.func.isRequired,
   };
 
-  onDeleteRow = (index, e) => {
-    const { dispatch, newOrder } = this.props;
+  constructor() {
+    super();
+    this.state = {
+      activeTab: '1',
+    };
+  }
 
-    e.preventDefault();
-    const arrayCopy = [...newOrder];
-    arrayCopy.splice(index, 1);
-    dispatch(deleteNewOrderTableData(arrayCopy));
+  toggle = (tab) => {
+    const { activeTab } = this.state;
+    if (activeTab !== tab) {
+      this.setState({
+        activeTab: tab,
+      });
+    }
   };
 
   render() {
-    const { newOrder, rtl } = this.props;
+    const { t } = this.props;
+    const { activeTab } = this.state;
 
     return (
       <Container className="dashboard">
         <Row>
-          <Col md={12}>
-            <h3 className="page-title">Gestión de Actividades</h3>
+          <Col md={12} className="dashboard__title-and-button">
+            <h3 className="page-title">{t('tasks.page_title')}</h3>
+            <Button color="success" className="dashboard__add-team icon">
+              <UserAddIcon /> {t('teams.add_team')}
+            </Button>
           </Col>
         </Row>
-        <Row>
-          <TotalProducts />
-          <TotalProfit />
-          <OrdersToday />
-          <Subscriptions />
-        </Row>
-        <Row>
-          <ProductSales rtl={rtl.direction} />
-          <BasicCard />
-          <SalesStatistic />
-          <MyTodos />
-          <SalesStatistisBar />
-          <SalesReport />
-          <Emails />
-          <ShortReminders />
-          <TopSellingProducts dir={rtl.direction} />
-          <NewOrders newOrder={newOrder} onDeleteRow={this.onDeleteRow} />
-          <RecentOrders />
-        </Row>
+        <Card>
+          <CardBody>
+            <div className="tabs tabs--justify tabs--bordered-bottom">
+              <div className="tabs__wrap">
+                <Nav tabs>
+                  <NavItem>
+                    <NavLink
+                      className={classnames({ active: activeTab === '1' })}
+                      onClick={() => {
+                        this.toggle('1');
+                      }}
+                    >
+                      {t('tasks.activities')}
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink
+                      className={classnames({ active: activeTab === '2' })}
+                      onClick={() => {
+                        this.toggle('2');
+                      }}
+                    >
+                      {t('tasks.map')}
+                    </NavLink>
+                  </NavItem>
+                </Nav>
+                <TabContent activeTab={activeTab}>
+                  <TabPane tabId="1">
+                    <p>Direction has strangers now believing. Respect enjoyed gay far exposed parlors towards. Enjoyment
+                      use tolerably dependent listening men. No peculiar in handsome together unlocked do by. Article
+                      concern joy anxious did picture sir her. Although desirous not recurred disposed off shy you
+                      numerous securing.
+                    </p>
+                  </TabPane>
+                  <TabPane tabId="2">
+                    <p>Direction has strangers now believing. Respect enjoyed gay far exposed parlors towards. Enjoyment
+                      use tolerably dependent listening men. No peculiar in handsome together unlocked do by.
+                    </p>
+                  </TabPane>
+                </TabContent>
+              </div>
+            </div>
+          </CardBody>
+        </Card>
       </Container>
     );
   }
 }
 
 export default connect(state => ({
-  newOrder: state.newOrder.items,
-  rtl: state.rtl,
+  rtl: state,
 }))(withTranslation('common')(Tasks));
